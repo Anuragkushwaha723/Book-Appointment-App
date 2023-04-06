131997411b2ddb1259da5602f1912d1599cfca1b
Anurag Kushwaha <anuragkushwaha723@gmail.com>
2023-04-06 14:57:55 +0530

    Connecting our own api to get,post and delete the user info

--- index.js
+++ index.js
@@ -1,59 +1,74 @@
-let form=document.querySelector('.my-form');
-form.addEventListener('submit',submitForm);
-window.addEventListener('DOMContentLoaded',()=>{
-    axios.get('https://crudcrud.com/api/e606adbdf9aa4239b892c2ca0af76017/userDetails')
-    .then(response=>{
-        for(let i=0;i<response.data.length;i++){
-            showOutput(response.data[i]);
-        }
-    })
-    .catch(err=>{console.log(err)});
+let form = document.querySelector('.my-form');
+form.addEventListener('submit', submitForm);
+window.addEventListener('DOMContentLoaded', () => {
+    axios.get('http://localhost:3000/user/get-user-data')
+        .then(response => {
+            for (let i = 0; i < response.data.length; i++) {
+                showOutput(response.data[i]);
+            }
+        })
+        .catch(err => { console.log(err) });
 });
 
 //submit form
-function submitForm(e){
+function submitForm(e) {
     e.preventDefault();
-    let name=document.querySelector('#name');
-    let email=document.querySelector('#email');
-    let val1=name.value;
-    let val2=email.value;
-    let objInfo={
-        name:val1,
-        email:val2,
+    let name = document.querySelector('#name');
+    let phone = document.querySelector('#phone');
+    let email = document.querySelector('#email');
+    let val1 = name.value;
+    let val2 = email.value;
+    let val3 = phone.value;
+    let objInfo = {
+        name: val1,
+        email: val2,
+        phone: val3
     }
-    axios.post('https://crudcrud.com/api/e606adbdf9aa4239b892c2ca0af76017/userDetails',objInfo)
-    .then(response=>showOutput(response.data))
-    .catch(err=>{console.log(err)});
-    name.value='';
-    email.value='';
+    axios.post('http://localhost:3000/user/add-user-data', objInfo)
+        .then(response => {
+            showOutput(response.data);
+        })
+        .catch(err => {
+            console.log(err)
+        });
+    name.value = '';
+    email.value = '';
+    phone.value = '';
 }
 
 //Delete button in list
-function delFun(id){
-    axios.delete(`https://crudcrud.com/api/e606adbdf9aa4239b892c2ca0af76017/userDetails/${id}`)
-    .then(res=>removeFromScreen(id))
-    .catch(err=>console.log(err));
+function delFun(id) {
+    axios.delete(`http://localhost:3000/user/delete-user-data/${id}`)
+        .then((res) => {
+            console.log(res.data.id);
+            removeFromScreen(res.data.id);
+        })
+        .catch(err => console.log(err));
+
 }
 
 //Edit button in List
-function editFun(id,val1,val2){
-    document.getElementById('name').value=val1;
-    document.getElementById('email').value=val2;
+function editFun(id, val1, val2, val3) {
+    document.getElementById('name').value = val1;
+    document.getElementById('email').value = val3;
+    document.getElementById('phone').value = val2;
     delFun(id);
 }
 
 //show  Output in the window
-function showOutput(data){
-    let parentNode=document.getElementById('lists');
-    let childNode=`<li id=${data._id}> ${data.name} - ${data.email} <button onclick=delFun('${data._id}')>Delete</button> <button onclick=editFun('${data._id}','${data.name}','${data.email}')>Edit</button></li>`;
-    parentNode.innerHTML=parentNode.innerHTML+childNode;
+function showOutput(data) {
+    let parentNode = document.getElementById('lists');
+    let childNode = `<li id=${data.id}> ${data.name} -  ${data.phone} - ${data.email} <button onclick=delFun('${data.id}')>Delete</button> <button onclick=editFun('${data.id}','${data.name}','${data.phone}','${data.email}')>Edit</button></li>`;
+    parentNode.innerHTML = parentNode.innerHTML + childNode;
 }
 
 //remove user from screen
-function removeFromScreen(id){
-    const parentNode=document.querySelector('#lists');
-    const childNode=document.getElementById(id);
-    if(childNode){
+function removeFromScreen(id) {
+    const parentNode = document.querySelector('#lists');
+    console.log(parentNode);
+    const childNode = document.getElementById(id);
+    console.log(childNode);
+    if (childNode) {
         parentNode.removeChild(childNode);
     }
 }
